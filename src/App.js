cddd5cb9c5adbcf79b773f5f9d23e4750125ae89
Dawid <dawidjura12@gmail.com>
2022-06-30 12:45:14 +0200

    Finished Footer section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Info from './components/Info.js';
 import About from './components/About.js';
+import Footer from './components/Footer.js'
 
 export default function App(){
     return (
         <div className="container">
             <Info />
             <About />
+            <Footer />
         </div>
     )
 }

c4884bdfa74b80542d3b7d339d3a7cfc9bbbff21
Dawid <dawidjura12@gmail.com>
2022-06-30 12:34:18 +0200

    Finished About section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Info from './components/Info.js';
+import About from './components/About.js';
 
 export default function App(){
     return (
         <div className="container">
             <Info />
+            <About />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import App from './App'
 import ReactDOM from 'react-dom/client'
 import "./style.css"
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
